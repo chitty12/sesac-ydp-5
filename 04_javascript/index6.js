811dda1ddff666ab6e90474168d471f1d8e92436
chitty12 <cty12angel@naver.com>
2023-07-27 19:22:14 +0900

    study : js 조건문 복습

--- 04_javascript/index6.js
+++ 04_javascript/index6.js
@@ -135,4 +135,4 @@ num % 2 === 1 ? console.log('홀수') : console.log('짝수');
 
 // 실습
 let now = new Date().getHours();
-now >= 12 ? console.log('오후') : console.log('오전');
+now <= 12 ? console.log('오전') : console.log('오후');
