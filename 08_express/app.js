76b0211ed434aadc245525f12f386afd009ef281
chitty12 <cty12angel@naver.com>
2023-08-11 14:49:15 +0900

    study : ejs 활용 학습

--- 08_express/app.js
+++ 08_express/app.js
@@ -6,11 +6,29 @@ const PORT = 8080;
 app.set('view engine', 'ejs'); // express 에서 사용할 템플릿 엔진 종류(ejs) 등록
 app.set('views', './views'); // 템플릿 엔진 파일을 저장할 위치를 등록
 
+// (가정) 데이터베이스에서 가져온 회원 정보(id, pw)임.
+const idFromDB = 'banana';
+const pwFromDB = '1234qwer';
+
 // get(경로, 해당 경로로 들어왔을 때 실행할 함수)
 // '/'(루트경로) : 서버주소의 포트번호(= localhost:8080/)
 app.get('/', function (req, res) {
   // res.send('<h1>Hello! Express!</h1>'); // send(x) x를 클라이언트에게 응답
-  res.render('index'); // render(ejs_filename) : ejs file 이름을 찾아서 클라이언트에게 응답
+  res.render('index', {
+    userId: idFromDB,
+    userPw: pwFromDB,
+    btns: ['사과', '오렌지', '키위'],
+    me: {
+      name: 'chitty',
+      msg: '안녕하세요 태영이에요',
+    },
+    isLogin: true,
+    title: ['로그인페이지', '회원가입페이지', '홈'],
+  }); // render(ejs_filename) : ejs file 이름을 찾아서 클라이언트에게 응답
+});
+
+app.get('/login', function (req, res) {
+  res.render('login');
 });
 
 // '/sesac' 경로 (http://localhost:8080/sesac)로 들어왔을 때, "새싹 영등포 캠퍼스 5기 수업중" 메세지 보이게하기
