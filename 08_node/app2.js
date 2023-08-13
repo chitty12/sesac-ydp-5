a2028473d26ac58dc8fb4a12073c6bc728f0593b
chitty12 <cty12angel@naver.com>
2023-08-13 18:14:00 +0900

    module 복습

--- 08_node/app2.js
+++ 08_node/app2.js
@@ -1,9 +1,9 @@
-const math = require('./math2');
+const add = require('./math2');
 
-console.log(math, typeof math);
-console.log(math.PI);
-console.log(math.E);
-console.log(math.add(2, 4));
+// console.log(math, typeof math);
+// console.log(math.PI);
+// console.log(math.E);
+console.log(add(2, 4));
 
 console.log(math.minus(4, 5));
 console.log(math.mltp(4, 5));
