7e26960c36396b19581641f2056253bb0cb24e3d
chitty12 <cty12angel@naver.com>
2023-08-16 18:41:27 +0900

    study: json

---
+++ 12_json/index.js
@@ -0,0 +1,25 @@
+// JSON
+const car = `{
+    "model": "IONIQ 5",
+    "company": "HYUNDAI",
+    "price": 50000000,
+    "year": 2023,
+    "isElectricCar": true,
+    "options": ["side mirror", "smart sensor", "built-in cam" ]
+}`;
+
+console.log(car);
+
+// JSON.parse() :json to js obj
+
+const obj = JSON.parse(car); // js obj 로 변환되었으니까...
+console.log(obj.model); // 연산자를 이용해서 추출(접근)가능하다
+console.log(obj.company);
+console.log(obj.wow);
+
+// JSON.stringify(): js obj to json
+const json = JSON.stringify(obj); // format : json
+console.log(json, typeof json);
+
+// json 변수는 JSON 형태를 가진 문자열일 뿐이므로, [] 연산자를 이용해 키 값 접근 불가능.
+console.log(json.company);
