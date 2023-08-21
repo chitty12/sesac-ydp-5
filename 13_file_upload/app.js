23addaa656f4198a7e5c9c9a20b79a9a1cec0a42
chitty12 <cty12angel@naver.com>
2023-08-21 15:32:39 +0900

    study : multer 세부 설정

--- 13_file_upload/app.js
+++ 13_file_upload/app.js
@@ -2,10 +2,28 @@ const express = require('express');
 const app = express();
 const PORT = 9000;
 const multer = require('multer');
+const path = require('path'); // 경로에 관한 내장 모듈
 
 const upload = multer({
   dest: 'uploads/',
   //클라이언트가 업로드한 파일을 저장할 서버측의 경로 속성.
+});
+
+const uploadDetail = multer({
+  storage: multer.diskStorage({
+    // storage: 저장할 공간에 대한 정보
+    destination(req, file, done) {
+      // done: callback 함수.
+      done(null, 'uploads/'); // 파일을 업로드할 경로 설정.
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname); // extname: 파일 "확장자"를 추출
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext); // basename: 파일 "이름"을 추출
+      // date.now()의 필요성 : 1. 중복 방지  2. 파일 이름만 보고 저장 시점 유추 가능
+      // null : error 를 의미하는 매개변수. 즉, 에러가 없으므로 'null' 이라고 전달하여 콜백 함수를 호출함.
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 파일 제한 정보 5MB
 });
 
 app.set('view engine', 'ejs');
@@ -21,9 +39,9 @@ app.get('/', (req, res) => {
 // 1. single () : 하나의 파일을 업로드할 때 사용하는 메서드
 // upload.single('userfile') : 클라이언트의 요청이 들어오면 multer 설정(upload 변수)에 따라 파일을 업로드 한후, req.file 객체 생성
 // single() 인자는 input 태그의 name 속성과 일치시켜야 함.
-app.post('/upload', upload.single('userfile'), (req, res) => {
+app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
   console.log(req.file); // req.file ; 파일 업로드 성공 결과 (파일 정보)
-  console.log(req.body); // req.body ; title 데이터 정보 확인 가능
+  console.log(req.body); // req.body ; 파일 외의 정보들
   res.send('파일 업로드 완료!');
 });
 
