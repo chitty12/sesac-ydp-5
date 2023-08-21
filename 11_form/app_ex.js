e10a351be1a818f83b085660fc1e5e666d3d5a8b
chitty12 <cty12angel@naver.com>
2023-08-21 18:53:49 +0900

    파일 업로드 실습

--- 11_form/app_ex.js
+++ 11_form/app_ex.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const multer = require('multer');
+const path = require('path');
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads/');
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(req.body.name) + ext);
+    },
+  }),
+});
 
 app.set('view engine', 'ejs');
 app.set('/views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.get('/', (req, res) => {
   res.render('get_ex');
@@ -15,6 +30,12 @@ app.get('/', (req, res) => {
 app.post('/postForm', (req, res) => {
   console.log(req.body);
   res.render('getresult_ex', { title: '회원가입 결과', userInfo: req.body });
+});
+
+app.post('/upload', upload.single('userfile'), (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+  res.send('upload!!');
 });
 
 app.get('/axios', (req, res) => {
