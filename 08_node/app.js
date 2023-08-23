062dac4dcfe788f7cbc372719485afc66a19376b
chitty12 <cty12angel@naver.com>
2023-08-24 01:26:48 +0900

    json 메서드 복습

--- 08_node/app.js
+++ 08_node/app.js
@@ -1,5 +1,10 @@
 // math 모듈을 불러오기
 
-const add = require('./math');
+// const add = require('./math');
 
-console.log(add(5, 6));
+// console.log(add(5, 6));
+
+var json_string =
+  '{ "name": "짱구", "gender": "m", "age": 7, "family": { "dog": "흰둥이",  "sister": "짱아" }}';
+var obj = JSON.parse(json_string);
+console.log(obj); // { name: "짱구", gender: "m", age: 7, family: { dog: "흰둥이",  sister: "짱아" }}
