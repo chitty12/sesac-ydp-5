5b22698fcd5ff94d70f70d96e4577f229f5ddac0
chitty12 <cty12angel@naver.com>
2023-08-28 15:53:10 +0900

    study: mvc with mysql로 read all 구현

--- 16_mvc_mysql/model/Visitor.js
+++ 16_mvc_mysql/model/Visitor.js
@@ -1,6 +1,25 @@
-exports.getVisitors = () => {
-  return [
-    { id: 1, name: '홍길동', comment: '내가 왔다' },
-    { id: 2, name: '이찬혁', comment: '으라차차' },
-  ];
+const mysql = require('mysql');
+const conn = {
+  host: 'localhost',
+  user: 'user1',
+  password: '1234',
+  database: 'visitor',
+};
+const connection = mysql.createConnection(conn);
+
+exports.getVisitors = (cb) => {
+  // [before]
+  //   return [
+  //     { id: 1, name: '홍길동', comment: '내가 왔다' },
+  //     { id: 2, name: '이찬혁', comment: '으라차차' },
+  //   ];
+
+  // [after]
+  connection.query('select * from visitor', (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    console.log(rows);
+    cb(rows);
+  });
 };
