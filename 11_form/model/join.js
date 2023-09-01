3b08dbf0fae6931e012913efd7de0ed943b6bbe2
chitty12 <cty12angel@naver.com>
2023-09-01 13:18:44 +0900

    split() '//'로 나누기

--- 11_form/model/join.js
+++ 11_form/model/join.js
@@ -1,7 +1,18 @@
-exports.tempUser = () => {
-  return [{ tempid: 'chitty', temppw: '1234' }];
-};
+// exports.tempUser = () => {
+//   return [{ tempid: 'chitty', temppw: '1234' }];
+// };
 
-exports.users = `apple//1234//사과사과
+// exports.users = `apple//1234//사과사과
+// banana//4321//바나나나나나
+// happy//qwer1234//해피해피`;
+
+const u = `apple//1234//사과사과
 banana//4321//바나나나나나
 happy//qwer1234//해피해피`;
+
+const users = u.split('\n');
+
+console.log(users);
+for (let user of users) {
+  user.split('//');
+}
