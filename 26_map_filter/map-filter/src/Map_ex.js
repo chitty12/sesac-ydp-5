776a03b2986e1f804fd176d68d10a4eb19a270c3
chitty12 <cty12angel@naver.com>
2023-10-05 09:37:27 +0900

    react map 실습 완성

--- 26_map_filter/map-filter/src/Map_ex.js
+++ 26_map_filter/map-filter/src/Map_ex.js
@@ -1,48 +1,89 @@
 import React, { useState } from 'react';
 
 export default function Map_ex() {
-  const [info, setInfo] = useState([
+  const [input, setInput] = useState([
     {
-      id: '',
-      name: '',
+      writer: '',
       title: '',
     },
   ]);
 
-  const writerInfo = () => {};
+  const [info, setInfo] = useState([]);
+
+  const { writer, title } = input;
+
+  const writerInfo = (e) => {
+    console.log(info);
+    const { value, name } = e.target;
+
+    console.log(name);
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const addInfo = () => {
+    console.log(writer);
+    const newInfo = { writer, title };
+    console.log(info);
+    setInfo([...info, newInfo]);
+    setInput({ ...input, writer: '', title: '' });
+  };
 
   return (
     <>
       <div>
-        <label for="name">작성자 : </label>
-        <input
-          type="text"
-          name="name"
-          placeholder="작성자"
-          id="name"
-          onChange={writerInfo}
-        ></input>
-        <label for="title">제목 : </label>
-        <input
-          type="text"
-          name="title"
-          placeholder="제목"
-          id="title"
-          onChange={writerInfo}
-        ></input>
-        <button>작성</button>
+        <fieldset>
+          <label htmlFor="writer">작성자 : </label>
+          <input
+            type="text"
+            name="writer"
+            placeholder="작성자"
+            id="writer"
+            onChange={writerInfo}
+            value={writer || ''}
+          ></input>
+          <label htmlFor="title">제목 : </label>
+          <input
+            type="text"
+            name="title"
+            placeholder="제목"
+            id="title"
+            value={title || ''}
+            onChange={writerInfo}
+          ></input>
+          <button onClick={addInfo}>작성</button>
+        </fieldset>
       </div>
 
-      <table border="1">
-        <tr>
-          <th>번호</th>
-        </tr>
-        <tr>
-          <th>제목</th>
-        </tr>
-        <tr>
-          <th>작성자</th>
-        </tr>
+      <select>
+        <option value="id">번호</option>
+        <option value="user">작성자</option>
+        <option value="title">제목</option>
+      </select>
+      <input type="text" placeholder="검색어" />
+      <button>검색</button>
+
+      <table border="1" style={{ width: '500px' }}>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+          </tr>
+        </thead>
+        <tbody>
+          {info.map((value, idx) => {
+            return (
+              <tr key={idx + 1}>
+                <td>{idx + 1}</td>
+                <td>{value.title}</td>
+                <td>{value.writer}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </>
   );
