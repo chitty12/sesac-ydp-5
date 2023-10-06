40e9efc85c78d06d51dd26d82071f6af552fd4d3
chitty12 <cty12angel@naver.com>
2023-10-06 13:06:59 +0900

    react map, filter, ref 종합실습

--- 26_map_filter/map-filter/src/Map_ex.js
+++ 26_map_filter/map-filter/src/Map_ex.js
@@ -5,13 +5,15 @@ export default function Map_ex() {
     {
       writer: '',
       title: '',
+      search: '',
     },
   ]);
   const [info, setInfo] = useState([]);
+  const [surf, setSurf] = useState();
   const refName = useRef();
   const refTitle = useRef();
 
-  const { writer, title } = input;
+  const { writer, title, search } = input;
 
   const writerInfo = (e) => {
     console.log(info);
@@ -86,8 +88,8 @@ export default function Map_ex() {
         <option value="user">작성자</option>
         <option value="title">제목</option>
       </select>
-      <input type="text" placeholder="검색어" />
-      <button>검색</button>
+      <input type="text" placeholder="검색어" value={surf} />
+      <button onClick={setSurf}>검색</button>
 
       <table border="1" style={{ width: '500px' }}>
         <thead>
