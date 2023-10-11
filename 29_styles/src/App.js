9110ede88c6b4200cf8a045a05058b198a7ec51d
chitty12 <cty12angel@naver.com>
2023-10-11 14:17:16 +0900

    study: react style 학습

---
+++ 29_styles/src/App.js
@@ -0,0 +1,14 @@
+import CssModuleComponents from './CssModuleComponents';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Styling</h1>
+      <h2>Css Module</h2>
+      <CssModuleComponents />
+    </div>
+  );
+}
+
+export default App;
